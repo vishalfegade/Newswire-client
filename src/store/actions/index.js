e86aa42f527b06f8c52c6c1e8c9ffe41066fdd37
vishalfegade <vsfegade2000@gmail.com>
2023-05-06 22:32:40 +0530

    updated code

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
+//* Backend server setup start
+// For development
+// const baseURL = 'http://localhost:3001'; // Change this to your Node.js server URL in development mode
+
+// For production
+const baseURL = 'https://newswire-server-9nfd.onrender.com'; // Change this to your Node.js server URL in production mode
+
+const api = axios.create({
+    baseURL: baseURL
+});
+//! End
+
 const getNewsItemsHelper = async () => {
 	try {
-		const response = await axios.get('/api/news');
+		const response = await api.get('/api/news');
 		return {
 			news: response.data
 		};
