b72047b0dd1618d539414b4db07c33ccc41a01ce
vishalfegade <vsfegade2000@gmail.com>
2023-05-07 16:54:42 +0530

    updated serverSetup.js

--- src/components/utils/serverSetup.js
+++ src/components/utils/serverSetup.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 //* Backend server setup start
 // For development
- const baseURL = 'http://localhost:3001'; // Change this to your Node.js server URL in development mode
+ //const baseURL = 'http://localhost:3001'; // Change this to your Node.js server URL in development mode
 
 // For production
-//const baseURL = 'https://newswire-server-9nfd.onrender.com'; // Change this to your Node.js server URL in production mode
+const baseURL = 'https://newswire-server-9nfd.onrender.com'; // Change this to your Node.js server URL in production mode
 
 const api = axios.create({
     baseURL: baseURL
